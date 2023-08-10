09fb6a0318bb8974301708315d045ecbbd3fadfb
goomon <jaegoo.jho@gmail.com>
2023-08-10 16:02:53 +0900

    이슈 조회 API (상태별 정렬)

--- issue-service/src/main/kotlin/issue/issueservice/web/IssueController.kt
+++ issue-service/src/main/kotlin/issue/issueservice/web/IssueController.kt
@@ -1,11 +1,14 @@
 package issue.issueservice.web
 
 import issue.issueservice.config.AuthUser
+import issue.issueservice.domain.enums.IssueStatus
 import issue.issueservice.model.IssueRequest
 import issue.issueservice.service.IssueService
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -19,4 +22,10 @@ class IssueController(
         authUser: AuthUser,
         @RequestBody request: IssueRequest,
     ) = issueService.create(authUser.userID, request)
+
+    @GetMapping
+    fun getAll(
+        authUser: AuthUser,
+        @RequestParam(required = false, defaultValue = "TODO") status: IssueStatus,
+    ) = issueService.getAll(status)
 }
