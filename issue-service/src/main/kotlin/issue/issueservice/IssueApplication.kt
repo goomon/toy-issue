7480f164461215b448b762c00a0b8880bee170e6
goomon <jaegoo.jho@gmail.com>
2023-08-10 12:10:52 +0900

    이슈 등록 API

--- issue-service/src/main/kotlin/issue/issueservice/IssueApplication.kt
+++ issue-service/src/main/kotlin/issue/issueservice/IssueApplication.kt
@@ -2,8 +2,10 @@ package issue.issueservice
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 
 @SpringBootApplication
+@EnableJpaAuditing
 class IssueApplication
 
 fun main(args: Array<String>) {
---
+++ issue-service/src/main/kotlin/issue/issueservice/domain/BaseEntity.kt
@@ -0,0 +1,23 @@
+package issue.issueservice.domain
+
+import jakarta.persistence.EntityListeners
+import jakarta.persistence.MappedSuperclass
+import jakarta.persistence.Temporal
+import jakarta.persistence.TemporalType
+import org.springframework.data.annotation.CreatedDate
+import org.springframework.data.annotation.LastModifiedDate
+import org.springframework.data.jpa.domain.support.AuditingEntityListener
+import java.time.LocalDateTime
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener::class)
+abstract class BaseEntity(
+
+    @CreatedDate
+    @Temporal(TemporalType.TIMESTAMP)
+    var createdAt: LocalDateTime? = null,
+
+    @LastModifiedDate
+    @Temporal(TemporalType.TIMESTAMP)
+    var updatedAt: LocalDateTime? = null,
+)
